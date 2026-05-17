376f44fccd8d83ea72dd8f0ae40eaa016036a139
Alasdey <lenalasdey@gmail.com>
2026-05-17 22:06:09 +0000

    uv instead of python calls

--- script_format.bash
+++ script_format.bash
@@ -3,7 +3,8 @@ export HF_HOME="/home/jovyan/buckets"
 read -s -r -p "Enter your Hugging Face token: " HF_TOKEN
 export HF_TOKEN
 
-python3 dataprep_llm_format.py --dataset Nofing/EventStoryLine-1.5-span
-python3 dataprep_llm_format.py --dataset Nofing/Hievents-span
-python3 dataprep_llm_format.py --dataset Nofing/MECI-v0.1-public-span
-python3 dataprep_llm_format.py --dataset Nofing/Maven-ERE-span
+uv run dataprep_llm_format.py --dataset Nofing/EventStoryLine-1.5-span
+uv run dataprep_llm_format.py --dataset Nofing/Hievents-span
+uv run dataprep_llm_format.py --dataset Nofing/MECI-v0.1-public-span
+uv run dataprep_llm_format.py --dataset Nofing/Maven-ERE-span
+uv run dataprep_llm_format.py --dataset Nofing/CausalTimeBank-aligned
